d198c2241ee38a16b2af7574e4e41714ef3cdf75
theplaceincan <abrahatatsbeha@gmail.com>
2026-05-09 11:58:55 -0700

    implement data and bss section initialization in Reset_Handler and fix linker script syntax

--- boot.c
+++ boot.c
@@ -10,9 +10,13 @@ extern uint32_t _edata;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
 
+// for test purposes
+volatile uint32_t initialized_global = 123;
+volatile uint32_t zero_global;
+
 __attribute__((section(".isr_vector")))
 const uint32_t vector_table[] = {
-  (uint32_t)&_estack, // set initial stack poionter to addr called e_stack
+  (uint32_t)&_estack, // set initial stack poionter to addr called _estack
   (uint32_t)Reset_Handler, // after reset, start executing at Reset_Handler
 };
 
@@ -28,6 +32,22 @@ int main(void) {
 // Zero .bss
 // Call main
 void Reset_Handler(void) {
+  uint32_t *src = &_sidata; // src points to initial data value in FLASH
+  uint32_t *dst = &_sdata; // dst points to start of .data in RAM
+
+  while (dst < &_edata) { // while less than end of .data
+    *dst = *src;
+    dst++;
+    src++;
+  }
+
+  dst = &_sbss;
+
+  while (dst < &_ebss) { // while less than end of .bss
+    *dst = 0;
+    dst++;
+  }
+
   main();
   while (1) {
   }
